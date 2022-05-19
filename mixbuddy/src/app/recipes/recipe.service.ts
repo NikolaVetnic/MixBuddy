67044b936f887030bc64e79e6a1ed117667108d8
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-05-19 13:08:01 +0200

    Services & dependency injection done.

---
+++ mixbuddy/src/app/recipes/recipe.service.ts
@@ -0,0 +1,36 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shoppint-list.service';
+
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test.',
+      'https://cdn.diffords.com/contrib/stock-images/2020/03/5e6276c0d9942.jpg',
+      [new Ingredient('Vodka', 1), new Ingredient('Cherries', 5)]
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply another test.',
+      'https://bakeitwithlove.com/wp-content/uploads/2021/10/Blue-Hawaiian-Cocktail-sq-500x500.jpg',
+      [new Ingredient('Schnaps', 1), new Ingredient('Apples', 3)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    // returns a new array which is a copy of the one stored here
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
--- mixbuddy/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ mixbuddy/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shoppint-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,9 +11,7 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
@@ -29,6 +21,6 @@ export class ShoppingEditComponent implements OnInit {
       this.amountInputRef.nativeElement.value
     );
 
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
   }
 }
