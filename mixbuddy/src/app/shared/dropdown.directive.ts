2c3e269bee39091e30adfbd06521a86388621aa0
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-05-19 12:13:07 +0200

    Dropdown directive done.

---
+++ mixbuddy/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  // add CSS class to its element once it's clicked and remove it once it's clicked again
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+  constructor(private elRef: ElementRef) {}
+}
