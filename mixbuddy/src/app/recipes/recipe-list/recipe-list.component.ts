23fb25579f290e1b8bb5b3b704e29c630a163f65
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-05-19 10:59:45 +0200

    Basics done.

---
+++ mixbuddy/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test.',
+      'https://cdn.diffords.com/contrib/stock-images/2020/03/5e6276c0d9942.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply another test.',
+      'https://bakeitwithlove.com/wp-content/uploads/2021/10/Blue-Hawaiian-Cocktail-sq-500x500.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
