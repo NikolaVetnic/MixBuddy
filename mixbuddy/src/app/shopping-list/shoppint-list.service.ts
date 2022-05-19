e183e176f3d315b259e5c9de388a4c36e84cb7a5
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-05-19 22:23:57 +0200

    Forms done.

--- mixbuddy/src/app/shopping-list/shoppint-list.service.ts
+++ mixbuddy/src/app/shopping-list/shoppint-list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -12,6 +13,10 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -26,5 +31,15 @@ export class ShoppingListService {
     this.ingredientsChanged.next(this.ingredients.slice());
 
     // TODO: add grouping of same ingredients if time allows
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
