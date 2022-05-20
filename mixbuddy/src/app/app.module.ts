edd5181a8e2e863f7d48c32388ff875e1b62a657
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-05-20 20:40:51 +0200

    Authentication done.

--- mixbuddy/src/app/app.module.ts
+++ mixbuddy/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +17,9 @@ import { ShoppingListService } from './shopping-list/shoppint-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -31,6 +34,8 @@ import { RecipeService } from './recipes/recipe.service';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AuthComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,7 +45,15 @@ import { RecipeService } from './recipes/recipe.service';
 
     AppRoutingModule,
   ],
-  providers: [RecipeService, ShoppingListService],
+  providers: [
+    RecipeService,
+    ShoppingListService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- mixbuddy/src/app/shared/data-storage.service.ts
+++ mixbuddy/src/app/shared/data-storage.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
   constructor(
+    private authService: AuthService,
     private httpClient: HttpClient,
     private recipeService: RecipeService
   ) {}
@@ -28,23 +30,34 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.httpClient
-      .get<Recipe[]>(
-        'https://mixbuddy-cdb00-default-rtdb.firebaseio.com/recipes.json'
-      )
-      .pipe(
-        map((recipes) => {
-          // first map - rxjs operator, second map - JS array method
-          return recipes.map((recipe) => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
-            };
-          });
-        }),
-        tap((recipes) => {
-          this.recipeService.setRecipes(recipes);
-        })
-      );
+    // take 1 value from observable and unsubscribe
+    // exhaustMap waits for the first observable (user) to complete
+    // entire observable chain then switches to the HTTP observable
+    return this.authService.user.pipe(
+      take(1),
+
+      exhaustMap((user) => {
+        return this.httpClient.get<Recipe[]>(
+          'https://mixbuddy-cdb00-default-rtdb.firebaseio.com/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token),
+          }
+        );
+      }),
+
+      map((recipes) => {
+        // first map - rxjs operator, second map - JS array method
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }
